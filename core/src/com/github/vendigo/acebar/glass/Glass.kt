9adf748d2b43a1128c9860f9a5fbe2a038c7265b
vendigo <ace.vendigo@gmail.com>
2017-08-18 16:41:15 +0300

    Draw glasses

--- core/src/com/github/vendigo/acebar/glass/Glass.kt
+++ core/src/com/github/vendigo/acebar/glass/Glass.kt
@@ -1,19 +1,10 @@
 package com.github.vendigo.acebar.glass
 
 import com.badlogic.gdx.graphics.Color
-import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable
 import com.github.vendigo.acebar.draw
 
-class Glass(val id: Int, val capacity: Int, var amount: Int) : Drawable {
-    private var leftWidth = 0f
-    private var rightWidth = 0f
-    private var topHeight = 0f
-    private var bottomHeight = 0f
-    private var minWidth = 0f
-    private var minHeight = 0f
-
+class Glass(val id: Int, val capacity: Int, var amount: Int) {
     var x: Float = 0f
     var y: Float = 0f
     val wallThickness = 4f
@@ -32,57 +23,5 @@ class Glass(val id: Int, val capacity: Int, var amount: Int) : Drawable {
             rectLine(x, y, x + width, y, wallThickness)
             rectLine(x + width, y, x + width, y + height, wallThickness)
         }
-    }
-
-    override fun draw(batch: Batch, x: Float, y: Float, width: Float, height: Float) {
-
-    }
-
-    override fun getLeftWidth(): Float {
-        return leftWidth
-    }
-
-    override fun setLeftWidth(leftWidth: Float) {
-        this.leftWidth = leftWidth
-    }
-
-    override fun getRightWidth(): Float {
-        return rightWidth
-    }
-
-    override fun setRightWidth(rightWidth: Float) {
-        this.rightWidth = rightWidth
-    }
-
-    override fun getTopHeight(): Float {
-        return topHeight
-    }
-
-    override fun setTopHeight(topHeight: Float) {
-        this.topHeight = topHeight
-    }
-
-    override fun getBottomHeight(): Float {
-        return bottomHeight
-    }
-
-    override fun setBottomHeight(bottomHeight: Float) {
-        this.bottomHeight = bottomHeight
-    }
-
-    override fun getMinWidth(): Float {
-        return minWidth
-    }
-
-    override fun setMinWidth(minWidth: Float) {
-        this.minWidth = minWidth
-    }
-
-    override fun getMinHeight(): Float {
-        return minHeight
-    }
-
-    override fun setMinHeight(minHeight: Float) {
-        this.minHeight = minHeight
     }
 }
