00ce64a0bfa82de8c3103026cdf734ac659e26ab
vendigo <ace.vendigo@gmail.com>
2017-08-18 18:38:10 +0300

    label in the scene

--- core/src/com/github/vendigo/acebar/AceBarGame.kt
+++ core/src/com/github/vendigo/acebar/AceBarGame.kt
@@ -2,7 +2,11 @@ package com.github.vendigo.acebar
 
 import com.badlogic.gdx.Game
 
+val WORLD_WIDTH = 640f
+val WORLD_HEIGHT = 480f
+
 class AceBarGame : Game() {
+
     override fun create() {
         setScreen(MainScreen())
     }
--- core/src/com/github/vendigo/acebar/MainScreen.kt
+++ core/src/com/github/vendigo/acebar/MainScreen.kt
@@ -2,23 +2,41 @@ package com.github.vendigo.acebar
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.ScreenAdapter
-import com.badlogic.gdx.graphics.Color
-import com.github.vendigo.acebar.glass.Glass
-import com.github.vendigo.acebar.table.Table
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle
+import com.badlogic.gdx.utils.viewport.FitViewport
 
 
 class MainScreen : ScreenAdapter() {
-    private lateinit var table: Table
+    private lateinit var stage: Stage
 
     override fun show() {
-        table = Table(listOf(
-                Glass(id = 1, capacity =  3, amount =  1),
-                Glass(id = 2, capacity =  5, amount =  3)
-        ))
+        stage = Stage(FitViewport(WORLD_WIDTH, WORLD_HEIGHT))
+        Gdx.input.inputProcessor = stage
+
+        val textStyle = LabelStyle()
+        textStyle.font = BitmapFont()
+
+        val label = Label("Welcome to Ace Bar!", textStyle)
+
+        label.addListener { event ->
+            if (event is InputEvent && event.type == InputEvent.Type.touchDown) {
+                println("Clicked!")
+            }
+            true
+        }
+        stage.addActor(label)
     }
 
     override fun render(delta: Float) {
-        Gdx.gl.glClearColor(Color.BLACK)
-        table.render()
+        stage.act(delta)
+        stage.draw()
+    }
+
+    override fun resize(width: Int, height: Int) {
+        stage.viewport.update(width, height)
     }
 }
